91a8b9e11393e185fd638b373101d35e3c6e85b7
gobacktosleep <egredimini@gmail.com>
2023-05-19 16:37:44 +0300

    improve tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,22 @@
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import path from 'path';
+import path, { dirname } from 'path';
 import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('compare two files', () => {
+test('compare two JSON files', () => {
   const result = gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const excpectedResult = readFile('expected_file.txt');
+  expect(result).toEqual(excpectedResult);
+});
+
+test('compare two YAML files', () => {
+  const result = gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'));
   const excpectedResult = readFile('expected_file.txt');
   expect(result).toEqual(excpectedResult);
 });
