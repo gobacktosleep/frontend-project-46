8d8a0cc0290837a88a0698cb7c62b7e3234dd505
gobacktosleep <egredimini@gmail.com>
2023-06-03 17:59:04 +0300

    add new test cases

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,15 +10,21 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const expectedResult = readFile('expected_file.txt');
+const expectedPlain = readFile('expected_plain.txt');
 
 const cases = [
   ['file1.json', 'file2.json', 'stylish', expectedResult],
   ['file1.yaml', 'file2.yaml', 'stylish', expectedResult],
   ['file1.yml', 'file2.yml', 'stylish', expectedResult],
+  ['file1.yaml', 'file2.yml', 'stylish', expectedResult],
+  ['file1.json', 'file2.yaml', 'stylish', expectedResult],
+  ['file1.json', 'file2.json', 'plain', expectedPlain],
+  ['file1.json', 'file2.yaml', 'plain', expectedPlain],
+  ['file1.yml', 'file2.json', 'plain', expectedPlain],
 ];
 
-describe.each(cases)('', (case1, case2, format, expectedFile) => {
-  test(`Gendiff test format ${format}`, () => {
+describe.each(cases)('Gendiff tests', (case1, case2, format, expectedFile) => {
+  test(`test for ${case1} & ${case2} with formatter ${format}`, () => {
     const result = gendiff(getFixturePath(case1), getFixturePath(case2), format, expectedFile);
     expect(result).toEqual(expectedFile);
   });
