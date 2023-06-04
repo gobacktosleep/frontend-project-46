57c23da2327f39c00728f3475e48a40f3ad26612
gobacktosleep <egredimini@gmail.com>
2023-06-04 17:12:25 +0300

    small fixes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import formatDiff from './formatters/index.js';
 
 const getFileFormat = (filepath) => path.extname(filepath).slice(1);
 const getData = (filepath) => getParsedData(readFileSync(filepath, 'utf-8'), getFileFormat(filepath));
-const getFilePath = (filepath) => path.resolve(process.cwd(), '__fixtures__', filepath);
+const getFilePath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = getData(getFilePath(filepath1));
